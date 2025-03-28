c04f47debf7f7a17b7ef82610c49eb5412e6c930
VIGHNESH SAWANT <sawantgauri310@gmail.com>
2025-03-28 13:05:38 +0530

    Add assignment 4

--- 4.py
+++ 4.py
@@ -1,3 +1,34 @@
 #Write a program in Python to create a binary file with name and roll number. Search for a given roll number and display the name, if not found display appropriate message.
 import pickle
 
+file=open("Text/student.dat",'ab')
+student=[]
+#taking input
+while True:
+    f=input("Do you want to add records? Enter y/n:")
+    if f.lower() != "y" :
+        break
+    n=input("Enter name of student:")
+    r=int(input("Enter rollno of student:"))
+    student.append((n,r))
+
+pickle.dump(student,file)
+file.close()
+#reading input
+student=[]
+fl=0
+file=open("Text/student.dat","rb")
+student=pickle.load(file)
+while True:
+    f=input("Do you want to search for records?Enter y/n:")
+    if f.lower() != "y":
+        break
+    r=int(input("Enter roll no to search:"))
+    for s in student:
+        
+        if s[1]==r:
+            fl=1
+            print(s[0])
+    if fl!=1:
+       print("No such student found")
+file.close()
